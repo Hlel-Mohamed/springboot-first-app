94a47625c53ed035775d579afaa0acb781473dbe
Mohamed Aziz Hlel <med.aziz.hlel@gmail.com>
2023-10-11 17:14:10 +0100

    REST API with Path Variable

--- src/main/java/com/springboot/app/StudentController.java
+++ src/main/java/com/springboot/app/StudentController.java
@@ -1,6 +1,7 @@
 package com.springboot.app;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -8,15 +9,10 @@ import java.util.List;
 
 @RestController
 public class StudentController {
-    // http://localhost:8080/students
-    @GetMapping("/students")
-    public List<Student> getStudents(){
-        List<Student> students = new ArrayList<>();
-        students.add(new Student("Aziz", "Hlel"));
-        students.add(new Student("Tony", "Cena"));
-        students.add(new Student("Sanjay", "Jadhav"));
-        students.add(new Student("Ram", "Jadhav"));
-        students.add(new Student("Umesh", "Fadatare"));
-        return students;
+    // http://localhost:8080/student/1
+    // @PathVariable annotation
+    @GetMapping("/student/{firstName}/{lastName}")
+    public Student studentPathVariable(@PathVariable("firstName") String firstName, @PathVariable("lastName") String lastName) {
+        return new Student(firstName, lastName);
     }
 }
