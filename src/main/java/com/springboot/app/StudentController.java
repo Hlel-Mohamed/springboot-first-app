c7f569e7d399a66e03e1ba43da878c4c4a753bf1
Mohamed Aziz Hlel <med.aziz.hlel@gmail.com>
2023-10-11 17:03:01 +0100

    REST API returns List

--- src/main/java/com/springboot/app/StudentController.java
+++ src/main/java/com/springboot/app/StudentController.java
@@ -2,11 +2,21 @@ package com.springboot.app;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
-    // http://localhost:8080/student
-    @GetMapping("/student")
-    public Student getStudent() {
-        return new Student("Aziz", "Hlel");
+    // http://localhost:8080/students
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Aziz", "Hlel"));
+        students.add(new Student("Tony", "Cena"));
+        students.add(new Student("Sanjay", "Jadhav"));
+        students.add(new Student("Ram", "Jadhav"));
+        students.add(new Student("Umesh", "Fadatare"));
+        return students;
     }
 }
